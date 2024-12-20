54a8b861c0afec43e6c0063c7f06baffe5ed97f5
florimhamaxhuni <151013335+florimhamaxhuni@users.noreply.github.com>
2024-12-20 23:17:15 +0100

    Added Orleans packages

--- src/MyWallet.Host/Program.cs
+++ src/MyWallet.Host/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.UseOrleans(static siloBuilder =>
+{
+    siloBuilder.UseLocalhostClustering();
+    siloBuilder
+        //.AddMemoryStreams("StreamProvider")
+        .AddMemoryGrainStorage("wallet");
+});
+
+var host = builder.Build();
+host.Run();
